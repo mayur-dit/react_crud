aa3c54da4d8afa5173d5963b4e8716d295893431
Mayur <mayur_dit@yahoo.co.in>
2019-10-26 19:11:16 +0530

    Mayur : React and other package updated to latest versin and fixed running errors.

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,21 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import Hello from './containers/Hello';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { enthusiasm } from './reducers/index';
-import { StoreState } from './types/index';
-
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {StoreState} from './types';
+import {enthusiasm} from './reducers';
 import './index.css';
 
+// @ts-ignore
 const store = createStore<StoreState>(enthusiasm, {
-  enthusiasmLevel: 1,
-  languageName: 'TypeScript',
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
 });
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Hello />
-  </Provider>,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <Hello/>
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
